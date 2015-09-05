f64cd22cb829dbf7c1450c1bacadfc25eb400b7b
Preston Ashton <thewarpdimension@gmail.com>
2015-09-05 16:11:17 +0100

    Update Manifest and Download Code

--- cl_init.lua
+++ cl_init.lua
@@ -1,5 +1,20 @@
-CommandPress = { Commands = {}, Helpers = {}, Version = 0.4 }
+CommandPress = { Commands = {}, Helpers = {}, Version = 0.4, BaseURL = "https://raw.githubusercontent.com/PrestonAshton/CommandPress/master/commandPress/" }
 
 me:PrintMessage(HUD_PRINTTALK, "Initialising CommandPress by Preston - Version " .. CommandPress.Version)
 
-http.Fetch("")
+http.Fetch(CommandPress.BaseURL .. "default.manifest", function(body, len, headers, code)
+	local lines = string.Explode("\n", body, false)
+	for i = 1,#lines do
+
+		http.Fetch(CommandPress.BaseURL .. lines[i],
+			function(body, len, headers, code)
+				RunStringEx(body, "CommandPress - " .. lines[i])
+			end,
+			function (error)
+				-- Error Handling
+			end)
+	end
+end,
+function(error)
+	-- Error Handling
+end)
