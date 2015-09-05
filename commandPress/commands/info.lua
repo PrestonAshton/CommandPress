11a922cdbdd6fa0dd30ef17bc672048b4a1e14ab
Preston Ashton <thewarpdimension@gmail.com>
2015-09-05 19:19:12 +0100

    Made info less spammy

--- commandPress/commands/info.lua
+++ commandPress/commands/info.lua
@@ -3,16 +3,33 @@ CommandPress:Add("info", function(text)
   local args = CommandPress:SplitText(text)
 
   local PrintFunc
+  local aloud = false
 
   if (args[2] == "aloud") then
     PrintFunc = function(text) Say(text) end
+    aloud = true
   else
     PrintFunc = function(text) CommandPress:Print(text) end
   end
 
 	PrintFunc("CommandPress by Preston - Version " .. CommandPress.Version)
 	PrintFunc("Currently loaded commands: ")
-	for k,v in pairs(CommandPress.Commands) do
-		PrintFunc(v[1])
-	end
+  if (aloud) then
+    local commandList = ""
+    local first = true
+    for k,v in pairs(CommandPress.Commands) do
+      if first then
+        commandList = commandList .. v[1]
+      else
+        commandList = commandList .. "," .. v[1]
+      end
+      first = false
+    end
+    commandList .. "."
+    PrintFunc(commandList)
+  else
+    for k,v in pairs(CommandPress.Commands) do
+      PrintFunc(v[1])
+    end
+  end
 end)
