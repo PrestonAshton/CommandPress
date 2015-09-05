a003442e761fa71dd544f46b330c821df1559312
Preston Ashton <thewarpdimension@gmail.com>
2015-09-05 17:43:09 +0100

    Fix commands in cl_init.lua

--- commandPress/cl_init.lua
+++ commandPress/cl_init.lua
@@ -3,8 +3,8 @@ hook.Add("PlayerSay", "textHook", function(ply, text)
 	for word in text:gmatch("%w+") do table.insert(args, word) end
 
 	if (text:sub(1, 1) == "!" or text:sub(1, 1) == "/")  then
-		if (commands[args[1]:sub(1, args[1]:len())]) then
-			local command = commands[args[1]:sub(1, args[1]:len())]
+		if (CommandPress:Commands[args[1]:sub(1, args[1]:len())]) then
+			local command = CommandPress:Commands[args[1]:sub(1, args[1]:len())]
 
 			if (command[2]) then
 				timer.Simple(0, function() command[2](ply, text) end );
