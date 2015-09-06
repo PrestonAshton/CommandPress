3e7849cb5a7d9c78822e21a8b492510c8516b8e8
Preston Ashton <thewarpdimension@gmail.com>
2015-09-06 15:42:20 +0100

    Wallhax on classname

--- commandPress/commands/wallhax.lua
+++ commandPress/commands/wallhax.lua
@@ -1,20 +1,32 @@
 local function Setup()
-  CommandPress:SetData("chatCmdWallHax",  "false")
   hook.Remove("PreDrawHalos", "chatCmdWallHaxHook")
+  CommandPress:SetData("chatCmdWallHax", "false")
 end
 
-CommandPress:Add("wallhax", function(ply, text)
+local function addHalos(entityName)
+  local haloColour = Color( 255, 0, 0 )
+  if (entityName == nil) then
+	  hook.Add( "PreDrawHalos", "chatCmdWallHaxHook", function()
+	    halo.Add(player.GetAll(), haloColour, 0, 0, 2, true, true )
+	  end )
+  else
+  	hook.Add( "PreDrawHalos", "chatCmdWallHaxHook", function()
+	    halo.Add(ents.FindByClass(entityName), haloColour, 0, 0, 2, true, true )
+	  end )
+  end
+
+  CommandPress:Print("Über Hax enabled!")
+  CommandPress:SetData("chatCmdWallHax", "true")
+end
+
+CommandPress:Add("wallhax", function(text)
 	if CommandPress:GetData("chatCmdWallHax", "false") == "false" then
-		CommandPress:Print(HUD_PRINTTALK, "Über Hax enabled!")
-    local haloColour = Color( 255, 0, 0 )
-		hook.Add( "PreDrawHalos", "chatCmdWallHaxHook", function()
-			halo.Add( player.GetAll(), haloColour, 0, 0, 2, true, true )
-		end )
-		CommandPress:SetData("chatCmdWallHax", "true")
+      local args = CommandPress:SplitText(text)
+      CommandPress:Print(args[2])
+      addHalos(args[2])
 	else
-		CommandPress:Print(HUD_PRINTTALK, "Über Hax disabled!")
-		hook.Remove("PreDrawHalos", "chatCmdWallHaxHook")
-		CommandPress:SetData("chatCmdWallHax", "false")
+    CommandPress:Print("Über Hax disabled!")
+    Setup()
 	end
 end)
 
