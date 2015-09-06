f35971bc8420707a0c7a34b33c6747b573dd1a93
Preston Ashton <thewarpdimension@gmail.com>
2015-09-06 18:12:20 +0100

    Define me and tell when initted

--- cl_init.lua
+++ cl_init.lua
@@ -2,13 +2,14 @@ function CommandPressSetup()
 
   CommandPress = {
     Commands = {},
-    Helpers = {},
-    Version = 0.4,
+    Helpers = {}, Version = 0.4,
     BaseURL = "https://raw.githubusercontent.com/PrestonAshton/CommandPress/master/commandPress/",
     Implementation = {
       Me = nil
     },
-   }
+  }
+
+  local me = LocalPlayer()
 
   me:PrintMessage(HUD_PRINTTALK, "Initialising CommandPress by Preston - Version " .. CommandPress.Version)
 
@@ -19,15 +20,20 @@ function CommandPressSetup()
   	  http.Fetch(CommandPress.BaseURL .. lines[i],
   		    function(body, len, headers, code)
   			    RunStringEx(body, "CommandPress - " .. CommandPress.BaseURL .. lines[i])
+	            if (i == #lines - 1) then
+	              me:PrintMessage(HUD_PRINTTALK, "CommandPress Initialised!")
+	            end
   	     end,
   		   function (error)
-  		     Say(error)
+  		     me:PrintMessage(HUD_PRINTTALK, error)
+  		     return
         end)
        end
   	end
   end,
   function(error)
-  	Say(error)
+  	me:PrintMessage(HUD_PRINTTALK, error)
+  	return
   end)
 
 end
