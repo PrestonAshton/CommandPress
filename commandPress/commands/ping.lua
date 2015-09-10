8a4be5cd9dc9c6e0c13a0ee02b663665dff1d0ef
Preston Ashton <thewarpdimension@gmail.com>
2015-09-10 23:13:08 +0100

    MY PING

--- commandPress/commands/ping.lua
+++ commandPress/commands/ping.lua
@@ -1,3 +1,6 @@
 CommandPress:Add("ping", function(text, ply)
+  if (ply == CommandPress:Me()) then
+    CommandPress:Say("Pong! My ping is: " .. ply:Ping())
+  end
   CommandPress:Say("Pong! Your ping is: " .. ply:Ping() .. ", " .. ply:Nick() .. ". Mine is: " .. CommandPress:Me():Ping())
 end)
