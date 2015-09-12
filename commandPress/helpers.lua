ddce246947bd5dfd0b848a0864b7254331c647f4
Preston Ashton <thewarpdimension@gmail.com>
2015-09-12 15:38:00 +0100

    Fix console commands' function arg ordering

--- commandPress/helpers.lua
+++ commandPress/helpers.lua
@@ -106,7 +106,7 @@ function CommandPress:Add(identifier, callback)
 	CommandPress.Commands[identifier] = {identifier, callback}
 	concommand.Add("cmdPress_" .. identifier, function(ply, _, __, args)
 		local text = "!" .. identifier .. " " .. args;
-		CommandPress.Commands[identifier][2](CommandPress:Me(), text)
+		CommandPress.Commands[identifier][2](text, CommandPress:Me())
 	end)
 end
 
