b565e8e2bd57466d8d459826f9283d718deea093
Preston Ashton <thewarpdimension@gmail.com>
2015-09-12 17:58:26 +0100

    Fake Ping

---
+++ commandPress/commands/fakeping.lua
@@ -0,0 +1,23 @@
+local function Setup()
+  CommandPress:SetData("chatCmdFakePing", "false")
+    CommandPress:SetData("chatCmdFakePingOriginalRate", GetConVar("cl_cmdrate"))
+end
+
+Setup()
+
+CommandPress:Add("fakeping", function(text, ply)
+
+  if (ply ~= CommandPress:Me()) then return end
+
+	if CommandPress:GetData("chatCmdFakePing", "false") == "false" then
+		ply:ConCommand("cl_cmdrate +100")
+    CommandPress:SetData("chatCmdFakePing", "true")
+	else
+		ply:ConCommand("cl_cmdrate " .. CommandPress:GetData("chatCmdFakePingOriginalRate", "66.7"))
+    CommandPress:SetData("chatCmdFakePing", "false")
+	end
+end)
+
+hook.Add("CommandPressCleanUp", "CleanAnHero", function()
+  Setup()
+end)
